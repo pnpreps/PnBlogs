b60e9d6c447cd2d565fa4c1919affb46b417df8f
praveen <praveen.lassopic@gmail.com>
2021-07-21 20:39:32 +0530

    blog completed

--- pnblogs/pnblogs/settings.py
+++ pnblogs/pnblogs/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
+    'blog.apps.BlogConfig',
     'portfolio.apps.PortfolioConfig',
     'djangocms_admin_style',
     'django.contrib.admin',
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -129,7 +132,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'pnblogs/static')
 ]
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
